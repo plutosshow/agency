ef640166e9c579d80ce3b53b016a17c707417077
yscherba <uscherba@evapp.ru>
2021-04-02 14:49:10 +0000

    slider fix

--- resources/views/templates/slider.blade.php
+++ resources/views/templates/slider.blade.php
@@ -3,8 +3,8 @@
         <div class="site-blocks-cover" data-aos="fade" data-stellar-background-ratio="0.5">
             <img src="{{asset('uploads/'.$slide->image)}}">
             <div class="text">
-                <h2>{{$slide->city}}, {{$slide->street}}</h2>
-                <p class="mb-2 small text-gray-600"><span class="property-icon icon-room"></span>{{$slide->district}}, {{$slide->city}}, {{$slide->zip}}</p>
+                <h2>{{$slide->city.","}} {{$slide->street}}</h2>
+                <p class="mb-2 small text-gray-600"><span class="property-icon icon-room"></span>{{$slide->district}} {{$slide->city.","}} {{$slide->zip}}</p>
                 <p class="mb-2"><strong>₽ {{ number_format($slide->price, 0, '', ' ') }}</strong></p>
                 <p class="mb-0"><a href="{{ route('property', $slide->id) }}" class="text-uppercase small letter-spacing-1 font-weight-bold">Подробнее</a>
                 </p>
